8b439ab521058e42a7b47b228063e226c154f543
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-10-29 12:11:41 +0900

    [FEAT] : ⚙️ fragment file name

--- app/src/main/java/com/example/alldayfit/exercisestatus/ExerciseStatusFragment.kt
+++ app/src/main/java/com/example/alldayfit/exercisestatus/ExerciseStatusFragment.kt
@@ -8,8 +8,9 @@ import android.view.ViewGroup
 import com.example.alldayfit.R
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.alldayfit.databinding.ExerciseStatusFragmentBinding
+import com.example.alldayfit.exercisestatus.adapter.DailyAdapter
 
 
 class ExerciseStatusFragment : Fragment() {
@@ -48,33 +49,46 @@ class ExerciseStatusFragment : Fragment() {
 
         binding.goalListFixBtn.setOnClickListener {
             val addGoalDialog =
-                ExerciseStatusDailyEditDialog(exerciseStatusViewModel)
+                ExerciseStatusAddGoalDialog(exerciseStatusAddGoalViewModel)
             addGoalDialog.show(childFragmentManager, "ExerciseStatusDailyEditDialog")
-            exerciseStatusViewModel.changePostType(exerciseStatusViewModel.dailyEditList)
+            exerciseStatusAddGoalViewModel.changePostType(exerciseStatusAddGoalViewModel.dailyEditList)
         }//프래그먼트 띄우기 및 타입 변환
 
         binding.goalList.layoutManager =
             androidx.recyclerview.widget.LinearLayoutManager(context)
-        adapter = DailyAdapter(exerciseStatusViewModel)
+        adapter = DailyAdapter(exerciseStatusAddGoalViewModel)
         binding.goalList.adapter = adapter
-        exerciseStatusViewModel.goalLiveData.observe(viewLifecycleOwner, Observer { data ->
+        exerciseStatusAddGoalViewModel.goalLiveData.observe(viewLifecycleOwner, Observer { data ->
             adapter.addGoal(data)
         })
         return binding.root
     }
 
     private fun initView() = with(binding) {
-        statusWeightView.statusTypeTxt.text=getString(R.string.weight)
-        statusHeightView.statusTypeTxt.text=getString(R.string.height)
-        statusBmiView.statusTypeTxt.text=getString(R.string.bmi)
-        statusExerciseTimeView.statusTypeTxt.text=getString(R.string.exercise_time)
-        statusCalorieConsumptionView.statusTypeTxt.text=getString(R.string.calorie_consumption)
+        /*  아이템 text 초기 설정*/
+        statusWeightView.statusTypeTxt.text = getString(R.string.weight)
+        statusHeightView.statusTypeTxt.text = getString(R.string.height)
+        statusBmiView.statusTypeTxt.text = getString(R.string.bmi)
+        statusExerciseTimeView.statusTypeTxt.text = getString(R.string.exercise_time)
+        statusCalorieConsumptionView.statusTypeTxt.text = getString(R.string.calorie_consumption)
         statusCalorieConsumptionView.statusTypeTxt.textSize = 13F
+
+        // 수정 버튼 클릭 시 해당 dialog 뜨위기
+        userConditionFixBtn.setOnClickListener {
+            showDialog(R.id.action_exerciseStatusFragment_to_exerciseStatusDailyEditDialog)
+        }
+        goalListFixBtn.setOnClickListener {
+            showDialog(R.id.action_exerciseStatusFragment_to_exerciseStatusAddGoalDialog)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    /* main_graph의 action을 활용해서 dialog 띄우기 */
+    private fun showDialog(action : Int) {
+        findNavController().navigate(action)
     }
 
     companion object {
