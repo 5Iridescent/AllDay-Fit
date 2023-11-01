4b9f4663710ae8d3550e04d0cf9d4c850b47afcd
ryuyeonjoo <yeonjoo3751@gmail.com>
2023-11-01 20:31:57 +0900

    타이머 기능 구현

--- app/src/main/java/com/example/alldayfit/count/CountPage.kt
+++ app/src/main/java/com/example/alldayfit/count/CountPage.kt
@@ -3,6 +3,9 @@ package com.example.alldayfit.count
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Chronometer
 import com.example.alldayfit.databinding.CountPageActivityBinding
@@ -10,14 +13,14 @@ import com.example.alldayfit.databinding.CountPageActivityBinding
 
 class CountPage : AppCompatActivity() {
     private lateinit var mBinding: CountPageActivityBinding
-    private var timer = false
-    private lateinit var chronometer: Chronometer
+    private var timerRunning = false
+    private var lastClickTime: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = CountPageActivityBinding.inflate(layoutInflater)
         val view = mBinding.root
         setContentView(view)
-        chronometer = mBinding.timer
 
         mBinding.setRootine.setOnClickListener {
             val countDialog = CountDialog()
@@ -25,32 +28,49 @@ class CountPage : AppCompatActivity() {
         }
 
         mBinding.count.setOnClickListener {
-            toggleStartWithClickVisibility()
-            if(!timer) {
-                startTimer()
-            } else {
-                stopTimer()
+            clickTimer()
+            val clickTime = System.currentTimeMillis()
+            if (clickTime - lastClickTime < DOUBLE_CLICK_TIME_DELTA) {
+                // 더블클릭 시 화면에 TextView 표시
+                mBinding.finishTextView.visibility = View.VISIBLE
+                mBinding.timer.visibility = View.INVISIBLE
             }
+            lastClickTime = clickTime
+        }
+
+        //시간을 00:00:00 으로 보이게 하는 코드
+        mBinding.timer.setOnChronometerTickListener {
+            val elapsedMillis = SystemClock.elapsedRealtime() - it.base
+            val hours = (elapsedMillis / 3600000).toInt()
+            val minutes = (elapsedMillis - hours * 3600000) / 60000
+            val seconds = (elapsedMillis - hours * 3600000 - minutes * 60000) / 1000
+
+            val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+            it.text = formattedTime
         }
     }
 
-    private fun toggleStartWithClickVisibility() {
-        // startWithClick TextView의 visibility를 토글
-        mBinding.textView.visibility = if (mBinding.textView.visibility == View.VISIBLE) {
-            View.GONE
+    private fun clickTimer() {
+        if (!timerRunning) {
+            startTimer()
+            mBinding.textView.visibility = View.GONE
+            mBinding.timer.visibility = View.VISIBLE
         } else {
-            View.VISIBLE
+            stopTimer()
         }
+        timerRunning = !timerRunning
     }
 
     private fun startTimer() {
         mBinding.timer.base = SystemClock.elapsedRealtime()
         mBinding.timer.start()
-        timer = true
     }
 
     private fun stopTimer() {
         mBinding.timer.stop()
-        timer = false
+    }
+
+    companion object {
+        private const val DOUBLE_CLICK_TIME_DELTA: Long = 300 // 더블클릭 간격 (밀리초)
     }
 }
