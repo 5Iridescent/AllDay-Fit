e6d3026300f73e9a886018e347b8b2afd06a1512
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-11-06 18:58:04 +0900

    [FEAT] : ⚙️ split reference user and all
    post는 전체를 가져와야 하니까 user와 전체 path references 분리

--- app/src/main/java/com/example/alldayfit/db/RealTimeRepository.kt
+++ app/src/main/java/com/example/alldayfit/db/RealTimeRepository.kt
@@ -1,28 +1,27 @@
 package com.example.alldayfit.db
 
-import android.content.Context
-import android.util.Log
+import com.example.alldayfit.community.model.CommunityPostEntity
 import com.example.alldayfit.db.model.FirebaseModel
 import com.example.alldayfit.utils.Util
-import com.google.firebase.FirebaseApp
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 interface RealTimeRepository {
 
-    fun getReference(path: String, id: String = userId): DatabaseReference {
+    fun getUserReference(path: String, id: String = userId): DatabaseReference {
         return database.reference.child(path).child(id)
+    }
+
+    fun getReference(path: String): DatabaseReference {
+        return database.reference.child(path)
     }
 
     private val userId: String
         get() = "NTqKsmydkVOE9fWxxTolGDSkL7p2"
     private val database: FirebaseDatabase
-        get() = FirebaseDatabase.getInstance(Util.realtime_database)
+        get() = Firebase.database(Util.realtime_database)
 
     /* 현 유저의 고유 user id를 가지고 user 테이블에 접근하여 데이터 가져오기 */
     fun getUserData() {}
