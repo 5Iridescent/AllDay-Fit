4f9ad82522c0402d8fdd36a0ab651969408c516a
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-11-15 00:13:53 +0900

    [FEAT] : ⚙️ add home toolbar action

--- app/src/main/java/com/example/alldayfit/MainActivity.kt
+++ app/src/main/java/com/example/alldayfit/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.alldayfit
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.alldayfit.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: MainActivityBinding
+    private val navHostFragment =
+        supportFragmentManager.findFragmentById(binding.mainFrame.id) as NavHostFragment
+    private val navController = navHostFragment.navController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +22,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initView() = with(binding) {
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(mainFrame.id) as NavHostFragment
-        val navController = navHostFragment.navController
         mainNav.setOnItemSelectedListener { item ->
             navController.navigate(item.itemId)
             true
@@ -29,7 +31,19 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.title = getString(R.string.app_title)
         supportActionBar?.title = title
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
         toolbarTitle.text = getString(R.string.app_title)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                navController.popBackStack()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/example/alldayfit/settings/ui/SettingGuideFragment.kt
+++ app/src/main/java/com/example/alldayfit/settings/ui/SettingGuideFragment.kt
@@ -2,9 +2,12 @@ package com.example.alldayfit.settings.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.alldayfit.databinding.SettingGuideFragmentBinding
 
 class SettingGuideFragment : Fragment() {
@@ -16,7 +19,12 @@ class SettingGuideFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = SettingGuideFragmentBinding.inflate(inflater, container, false)
+        initView()
         return binding.root
+    }
+
+    private fun initView() {
+        activity?.actionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onDestroyView() {
